b8c89c9fb0f5df8d4844e75e66ea42bbfaf26733
PernDev <Pern.dev@gmail.com>
2022-10-13 11:08:09 +0700

    💄lotter form create

--- components/LotteryForm/index.stories.js
+++ components/LotteryForm/index.stories.js
@@ -1,7 +1,6 @@
 import LotteryForm from "."
 
 
-<<<<<<< HEAD
 // export const LotteryFormStory = (args) => (
 //     <LotteryForm method="post" {...args} />
 //   );
@@ -15,14 +14,3 @@ import LotteryForm from "."
 // }
 
 
-=======
-export const Story = (args) => <LotteryForm { ...args }/>;
-
-
-export default {
-  title: 'LotteryForm/components/LotteryForm',
-  component: LotteryForm,
-  argTypes:  {
-  }
-}
->>>>>>> a5eddb5654c34ffb77012b5cb6f8ebf0be69bba4
