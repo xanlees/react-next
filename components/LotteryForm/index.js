15c7a7e7340b8e00311599bb46fda44361750ef6
PernDev <Pern.dev@gmail.com>
2022-10-15 18:52:32 +0700

    create form lottery_day

--- components/LotteryForm/index.js
+++ components/LotteryForm/index.js
@@ -38,122 +38,125 @@ const index = ({ url, method }) => {
       code,
       user: 1,
     };
-   
 
-  return (
-    <div>
-      <form onSubmit={handleSubmit} method="post" encType="multipart/form-data">
-        {/* Set language */}
-        <div className="max-w-lg  bg-gray-100  shadow-2xl mx-auto text-center">
-          <h1 className="text-gray-900">Language</h1>
-          <div className="container py-2 max-w-md mx-auto flex flex-col-2 justify-center">
-            <div className="bg-gray-200 text-lg  p-1 rounded-md">
-              <input
-                id="en"
-                type="radio"
-                name="language"
-                value="en"
-                onChange={handleChangeLanguage}
-                className="peer hidden"
-                defaultChecked
-              />
-              <label
-                className={`block cursor-pointer select-none rounded-xl p-3 text-center dark:text-white  peer-checked:bg-sky-500 peer-checked:font-bold peer-checked:text-white${
-                  active === 1 ? "bg-sky-400 rounded-lg" : ""
-                } dark:text-white px-1 py-1 hover:bg-sky-400 hover:rounded-lg`}
-                onClick={() => setActive(1)}
-                htmlFor="en"
-              >
-                English
-              </label>
-            </div>
-            <div className="bg-gray-200 ml-5 text-lg p-1 rounded-md">
-              <input
-                type="radio"
-                name="language"
-                value="la"
-                className="peer hidden"
-                id="la"
-                onChange={handleChangeLanguage}
-              />
-              <label
-                className={`block cursor-pointer select-none rounded-xl p-3 text-center dark:text-white  peer-checked:bg-sky-500 peer-checked:font-bold peer-checked:text-white${
-                  active === 2 ? "bg-sky-400 rounded-lg" : ""
-                } px-1 py-1 hover:bg-sky-400 hover:rounded-lg`}
-                onClick={() => setActive(2)}
-                htmlFor="la"
-              >
-                Lao
-              </label>
-            </div>
-          </div>
-        </div>
-        {/* Set lottery info */}
-        <div className="max-w-lg  bg-gray-100 shadow-2xl rounded-lg m-auto text-center py-12 mt-4 ">
-          <div className="mx-5">
-            <h1 className="text-gray-900 text-center font-extrabold mt-3 text-3xl mb-5">
-              Set Lottery
-            </h1>
-            <div className="grid gap-6 mb-6 lg:grid-cols-2">
-              <div>
-                <label
-                  htmlFor="lottery_name"
-                  className="block mb-2 text-sm font-medium dark:text-gray-900 text-left"
-                >
-                  Lottery Name
-                </label>
+    return (
+      <div>
+        <form
+          onSubmit={handleSubmit}
+          method="post"
+          encType="multipart/form-data"
+        >
+          {/* Set language */}
+          <div className="max-w-lg  bg-gray-100  shadow-2xl mx-auto text-center">
+            <h1 className="text-gray-900">Language</h1>
+            <div className="container py-2 max-w-md mx-auto flex flex-col-2 justify-center">
+              <div className="bg-gray-200 text-lg  p-1 rounded-md">
                 <input
-                  type="text"
-                  id="lottery_name"
-                  name="name"
-                  className="border border-gray-300 text-sm rounded-lg focus:ring-sky-500 focus:border-sky-500 block w-full p-2.5 bg-sky-400 dark:placeholder-gray-400 dark:text-white dark:focus:ring-sky-500 dark:focus:border-sky-500"
-                  onChange={(e) => handleChangeTrans(e)}
+                  id="en"
+                  type="radio"
+                  name="language"
+                  value="en"
+                  onChange={handleChangeLanguage}
+                  className="peer hidden"
+                  defaultChecked
                 />
+                <label
+                  className={`block cursor-pointer select-none rounded-xl p-3 text-center dark:text-white  peer-checked:bg-sky-500 peer-checked:font-bold peer-checked:text-white${
+                    active === 1 ? "bg-sky-400 rounded-lg" : ""
+                  } dark:text-white px-1 py-1 hover:bg-sky-400 hover:rounded-lg`}
+                  onClick={() => setActive(1)}
+                  htmlFor="en"
+                >
+                  English
+                </label>
               </div>
-              <div>
-                <label
-                  htmlFor="code_name"
-                  className="block mb-2 text-sm font-medium dark:text-gray-900 text-left"
-                >
-                  Code name
-                </label>
+              <div className="bg-gray-200 ml-5 text-lg p-1 rounded-md">
                 <input
-                  type="text"
-                  id="code_name"
-                  name="code"
-                  className="border border-gray-300 text-sm rounded-lg focus:ring-sky-500 focus:border-sky-500 block w-full p-2.5 bg-sky-400 dark:placeholder-gray-400 dark:text-white dark:focus:ring-sky-500 dark:focus:border-sky-500"
-                  onChange={(e) => setCode(e.target.value)}
+                  type="radio"
+                  name="language"
+                  value="la"
+                  className="peer hidden"
+                  id="la"
+                  onChange={handleChangeLanguage}
                 />
+                <label
+                  className={`block cursor-pointer select-none rounded-xl p-3 text-center dark:text-white  peer-checked:bg-sky-500 peer-checked:font-bold peer-checked:text-white${
+                    active === 2 ? "bg-sky-400 rounded-lg" : ""
+                  } px-1 py-1 hover:bg-sky-400 hover:rounded-lg`}
+                  onClick={() => setActive(2)}
+                  htmlFor="la"
+                >
+                  Lao
+                </label>
               </div>
             </div>
-            <div className="mb-6">
-              <label
-                htmlFor="password"
-                className="block mb-2 text-sm font-medium dark:text-gray-900 text-left"
-              >
-                Image
-              </label>
-              <input
-                type="file"
-                id="password"
-                className="border border-gray-300 text-sm rounded-lg focus:ring-sky-400 focus:border-sky-100 block w-full p-2.5 bg-sky-400 dark:placeholder-gray-400 dark:text-white dark:focus:ring-sky-400 dark:focus:border-sky-400"
-                onChange={onFileChange}
-              />
+          </div>
+          {/* Set lottery info */}
+          <div className="max-w-lg  bg-gray-100 shadow-2xl rounded-lg m-auto text-center py-12 mt-4 ">
+            <div className="mx-5">
+              <h1 className="text-gray-900 text-center font-extrabold mt-3 text-3xl mb-5">
+                Set Lottery
+              </h1>
+              <div className="grid gap-6 mb-6 lg:grid-cols-2">
+                <div>
+                  <label
+                    htmlFor="lottery_name"
+                    className="block mb-2 text-sm font-medium dark:text-gray-900 text-left"
+                  >
+                    Lottery Name
+                  </label>
+                  <input
+                    type="text"
+                    id="lottery_name"
+                    name="name"
+                    className="border border-gray-300 text-sm rounded-lg focus:ring-sky-500 focus:border-sky-500 block w-full p-2.5 bg-sky-400 dark:placeholder-gray-400 dark:text-white dark:focus:ring-sky-500 dark:focus:border-sky-500"
+                    onChange={(e) => handleChangeTrans(e)}
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="code_name"
+                    className="block mb-2 text-sm font-medium dark:text-gray-900 text-left"
+                  >
+                    Code name
+                  </label>
+                  <input
+                    type="text"
+                    id="code_name"
+                    name="code"
+                    className="border border-gray-300 text-sm rounded-lg focus:ring-sky-500 focus:border-sky-500 block w-full p-2.5 bg-sky-400 dark:placeholder-gray-400 dark:text-white dark:focus:ring-sky-500 dark:focus:border-sky-500"
+                    onChange={(e) => setCode(e.target.value)}
+                  />
+                </div>
+              </div>
+              <div className="mb-6">
+                <label
+                  htmlFor="password"
+                  className="block mb-2 text-sm font-medium dark:text-gray-900 text-left"
+                >
+                  Image
+                </label>
+                <input
+                  type="file"
+                  id="password"
+                  className="border border-gray-300 text-sm rounded-lg focus:ring-sky-400 focus:border-sky-100 block w-full p-2.5 bg-sky-400 dark:placeholder-gray-400 dark:text-white dark:focus:ring-sky-400 dark:focus:border-sky-400"
+                  onChange={onFileChange}
+                />
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="max-w-lg m-auto text-center py-5 mt-4 ">
-          <button
-            type="submit"
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          >
-            Save
-          </button>
-        </div>
-      </form>
-    </div>
-  );
+          <div className="max-w-lg m-auto text-center py-5 mt-4 ">
+            <button
+              type="submit"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Save
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  };
 };
-
 export default index;
