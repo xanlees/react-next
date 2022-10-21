11c63e8c30ebd3820bc256250c2845dcecafbd0d
PernDev <Pern.dev@gmail.com>
2022-10-21 14:10:20 +0700

    Lottery list for user

--- pages/customer/index.js
+++ pages/customer/index.js
@@ -2,14 +2,12 @@ import React from "react";
 import CustomerLayout from "../../layouts/CustomerLayout";
 import LotteryList from "../../components/LotteryList";
 import getAPI from "../../components/LotteryList/uitil";
-// import Lottery from "../../components/LotteryForm";
 
 export default function index(props) {
   return (
     <>
       <CustomerLayout>
         <LotteryList {...props} />
-        {/* <Lottery /> */}
       </CustomerLayout>
     </>
   );
