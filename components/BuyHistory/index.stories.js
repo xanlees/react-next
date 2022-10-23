bbf3a9dc332177f29899a8408734c4611c127d6f
PernDev <Pern.dev@gmail.com>
2022-10-23 14:12:50 +0700

    add websit storybook vechaiUI

---
+++ components/BuyHistory/index.stories.js
@@ -0,0 +1,9 @@
+import BuyHistory from ".";
+
+export const Story = (args) => <BuyHistory {...args} />;
+
+export default {
+  title: "BuyHistory",
+  component: BuyHistory,
+  argTypes: {},
+};
--- components/BuyLottery/index.stories.js
+++
@@ -1,9 +0,0 @@
-import BuyLottery from ".";
-
-export const Story = (args) => <BuyLottery {...args} />;
-
-export default {
-  title: "BuyLottery",
-  component: BuyLottery,
-  argTypes: {},
-};
