cadf39a4391ec71fd8cdbd5141bc552e1b76967b
PernDev <Pern.dev@gmail.com>
2022-10-24 15:03:50 +0700

    add component lottry list

--- pages/admin/predict-payment-lose/index.js
+++ pages/admin/predict-payment-lose/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import AdminLayout from "../../../layouts/AdminLayout";
-import PredictTwoNumber from "../../../components/Predictpaymentlose/Predictpaymentlose_2";
+// import PredictTwoNumber from "../../../components/Predictpaymentlose/Predictpaymentlose_2";
 import getAPI_Lower from "../../../components/Predictpaymentlose/Predictpaymentlose_2/Lower_2/util";
 // import getAPI_Upper from "../../../components/Predictpaymentlose/Predictpaymentlose_2/Upper_2/util";
 
