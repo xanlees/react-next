4f97ef9b06e5db9adb3f50f658943631ae11953c
PernDev <Pern.dev@gmail.com>
2022-10-28 15:27:22 +0700

    create page predict lose agent

---
+++ pages/agent/predict-lose/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import AgentLayout from "../../../layouts/AgentLayout";
+import Predictpaymentlose_Evennumber_Primenumber from "../../../components/Predictpaymentlose/Predictpaymentlose_Evennumber_Primenumber";
+
+const index = (prop) => {
+  return (
+    <AgentLayout>
+      <Predictpaymentlose_Evennumber_Primenumber {...prop} />
+    </AgentLayout>
+  );
+};
+
+export async function getServerSideProps() {
+  return getAPI();
+}
+
+export default index;
