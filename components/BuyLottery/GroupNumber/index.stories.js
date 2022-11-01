7321ec9c04b74006f7a7cb4890140bf68e41b38c
PernDev <Pern.dev@gmail.com>
2022-11-01 12:15:59 +0700

    edit period

--- components/BuyLottery/GroupNumber/index.stories.js
+++ components/BuyLottery/GroupNumber/index.stories.js
@@ -1,24 +1,9 @@
-<<<<<<< HEAD
-import GroupNumber from "."
-
-
-export const Story = (args) => <GroupNumber { ...args }/>;
-
-
-export default {
-  title: 'BuyLottery/component/GroupNumber',
-  component: GroupNumber,
-  argTypes:  {
-  }
-}
-=======
 import GroupNumber from ".";
 
 export const Story = (args) => <GroupNumber {...args} />;
 
 export default {
-  title: "BuyLottery/GroupNumber",
+  title: "BuyLottery/component/GroupNumber",
   component: GroupNumber,
   argTypes: {},
 };
->>>>>>> a8bf83527cc9077b09db451df51944851d1979af
--- pages/admin/predict-payment-lose/index.js
+++ pages/admin/predict-payment-lose/index.js
@@ -1,13 +1,7 @@
 import React from "react";
 import AdminLayout from "../../../layouts/AdminLayout";
-<<<<<<< HEAD
 import PredictTwoNumber from "../../../components/Predictpaymentlose/Predictpaymentlose_two";
 // import getAPI_Lower from "../../../components/Predictpaymentlose/Predictpaymentlose_two/";
-=======
-// import PredictTwoNumber from "../../../components/Predictpaymentlose/Predictpaymentlose_2";
-import getAPI_Lower from "../../../components/Predictpaymentlose/Predictpaymentlose_2/Lower_2/util";
-// import getAPI_Upper from "../../../components/Predictpaymentlose/Predictpaymentlose_2/Upper_2/util";
->>>>>>> ea668275925642d20189a5ee91da3e436ad91b3d
 
 const index = (lower) => {
   return (
